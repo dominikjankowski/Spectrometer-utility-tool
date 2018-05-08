72e7ca0387b6b1fb5ecb9834362681013b1d1307
Dominik Jankowski <dominik.jankowsk@gmail.com>
2018-05-08 23:16:18 +0200

    dzień trzeci koniec

--- script.js
+++ script.js
@@ -1,44 +1,38 @@
-//var input = document.querySelector("input");
-//var preview = document.querySelector(".preview");
-//
-//input.addEventListener('change', updateFileDisplay);
-//
-//function updateFileDisplay() {
-//  while(preview.firstChild) {
-//    preview.removeChild(preview.firstChild);
-//  }
-//    
-//var curFiles = input.files;
-//    
-//if(curFiles.length === 0) {
-//    var para = document.createElement('p');
-//    para.textContent = 'No files currently selected for upload';
-//    preview.appendChild(para);
-//  } else {
-//    var list = document.createElement('ol');
-//    preview.appendChild(list);
-//    for(var i = 0; i < curFiles.length; i++) {
-//    var listItem = document.createElement('li');
-//    var para = document.createElement('p');
-//    if(validFileType(curFiles[i])) {
-//    para.textContent = 'File name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
-//    var plik;
-//    plik.src = window.URL.createObjectURL(curFiles[i]);
-//    
-//    listItem.appendChild(plik);
-//    listItem.appendChild(para);
-//    } else {
-//        para.textContent = 'File name ' + curFiles[i].name + ': Not a valid file type. Update your selection.';
-//        listItem.appendChild(para);
-//      }
-//
-//      list.appendChild(listItem);
-//    }
-//  }
-//}
-//
+var input = document.querySelector('input');
+var preview = document.querySelector('.preview');
+
+input.addEventListener('change', updateFunctionDisplay);
+
+function updateFunctionDisplay() {
+    
+  while(preview.firstChild) {
+    preview.removeChild(preview.firstChild);
+  }
+
+  var curFiles = input.files;
+  if(curFiles.length === 0) {
+    var para = document.createElement('p');
+    para.textContent = 'No files currently selected for upload';
+    preview.appendChild(para);
+  } else {
+    var list = document.createElement('p');
+    for(var i = 0; i < curFiles.length; i++) {
+     
+      if(validFileType(curFiles[i])) {
+        list.textContent = 'File name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
+      } else {
+        list.textContent = 'File name ' + curFiles[i].name + ': Not a valid file type. Update your selection.';
+      }
+       preview.appendChild(list);
+
+    }
+  }
+}
+
 //var fileTypes = [
-//  ".html"
+//  'image/jpeg',
+//  'image/pjpeg',
+//  'image/png'
 //]
 //
 //function validFileType(file) {
@@ -50,67 +44,8 @@
 //
 //  return false;
 //}
-//
-//function returnFileSize(number) {
-//  if(number < 1024) {
-//    return number + 'bytes';
-//  } else if(number >= 1024 && number < 1048576) {
-//    return (number/1024).toFixed(1) + 'KB';
-//  } else if(number >= 1048576) {
-//    return (number/1048576).toFixed(1) + 'MB';
-//  }
-//}
-var input = document.querySelector('input');
-var preview = document.querySelector('.preview');
 
-input.style.opacity = 0;
-input.addEventListener('change', updateImageDisplay);function updateImageDisplay() {
-  while(preview.firstChild) {
-    preview.removeChild(preview.firstChild);
-  }
-
-  var curFiles = input.files;
-  if(curFiles.length === 0) {
-    var para = document.createElement('p');
-    para.textContent = 'No files currently selected for upload';
-    preview.appendChild(para);
-  } else {
-    var list = document.createElement('ol');
-    preview.appendChild(list);
-    for(var i = 0; i < curFiles.length; i++) {
-      var listItem = document.createElement('li');
-      var para = document.createElement('p');
-      if(validFileType(curFiles[i])) {
-        para.textContent = 'File name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
-        var image = document.createElement('img');
-        image.src = window.URL.createObjectURL(curFiles[i]);
-
-        listItem.appendChild(image);
-        listItem.appendChild(para);
-
-      } else {
-        para.textContent = 'File name ' + curFiles[i].name + ': Not a valid file type. Update your selection.';
-        listItem.appendChild(para);
-      }
-
-      list.appendChild(listItem);
-    }
-  }
-}
-
-var fileTypes = [
-  "\.html"
-];
-
-function validFileType(file) {
-  for(var i = 0; i < fileTypes.length; i++) {
-    if(file.type === fileTypes[i]) {
-      return true;
-    }
-  }
-
-  return false;
-}function returnFileSize(number) {
+function returnFileSize(number) {
   if(number < 1024) {
     return number + 'bytes';
   } else if(number >= 1024 && number < 1048576) {
